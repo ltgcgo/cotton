ad588ee418d2d28f756dd0f39888bb4affb6b71a
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2025-06-25 09:16:53 +0000

    Experimental publish.

--- sh/submit.sh
+++ sh/submit.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 SERVICE=$(cat "./conf/service")
 ls -1 "./build/"; while IFS= read -r folder; do
+	echo "Publishing \"${folder}\"..."
 	podman push "${SERVICE}_${folder}_latest" "${1}:${folder}"
 done
 exit
