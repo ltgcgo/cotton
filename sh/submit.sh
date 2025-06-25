153770cdcdfa23ad73f3cc1e9f6b327e8f59f3b7
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2025-06-25 09:23:23 +0000

    Please do something?

--- sh/submit.sh
+++ sh/submit.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 SERVICE=$(cat "./conf/service")
-ls -1 "./build/"; while IFS= read -r folder; do
-	echo "Publishing \"${folder}\"..."
-	podman push "${SERVICE}_${folder}_latest" "${1}:${folder}"
+echo "Container image submission has started."
+ls -1 "./build/" | while IFS= read -r folder; do
+	imageName="${SERVICE}_${folder}_latest"
+	echo "Publishing \"${folder}\" from \"${imageName}\"..."
+	podman push "${imageName}" "${1}:${folder}"
 done
 exit
